1a88bff1d935941b70da6188f409ad4c24ecf07e
visserle <leonard.visser@mailbox.org>
2023-12-04 13:04:35 +0100

    refactor

---
+++ src/experiments/psychopy_import.py
@@ -0,0 +1,27 @@
+import logging
+import importlib
+import subprocess
+import sys
+
+logger = logging.getLogger(__name__.rsplit(".", maxsplit=1)[-1])
+
+def psychopy_import(package_name: str):
+    """
+    Helper function to import/install a package in a PsychoPy environment.
+
+    If the specified package is not found in the psychopy env, the script attempts to install it using pip.
+
+    Note: If you are running this function for the first time in a psychopy script, it will most likely throw an error. Simply run the script again and it should work.
+    """
+    try:
+        return importlib.import_module(package_name)
+    except ImportError:
+        try:
+            # Try to install the package using pip in a subprocess
+            process = subprocess.run([sys.executable, "-m", "pip", "install", package_name], check=True)
+            if process.returncode != 0:
+                raise Exception("pip installation failed")
+            return importlib.import_module(package_name)
+        except Exception as exc:
+            logger.error(f"Failed to install and import '{package_name}': {exc}")
+            raise
--- src/experiments/psychopy_utils.py
+++ src/experiments/psychopy_utils.py
@@ -1,37 +1,13 @@
 # work in progress
 
 import logging
-import importlib
-import subprocess
-import sys
 import tkinter as tk
 from tkinter import messagebox
 
+from src.experiments.psychopy_import import psychopy_import
+screeninfo = psychopy_import("screeninfo")
 
 logger = logging.getLogger(__name__.rsplit(".", maxsplit=1)[-1])
-
-def psychopy_import(package_name: str):
-    """
-    Helper function to import/install a package in a PsychoPy environment.
-
-    If the specified package is not found in the psychopy env, the script attempts to install it using pip.
-
-    Note: If you are running this function for the first time in a psychopy script, it will most likely throw an error. Simply run the script again and it should work.
-    """
-    try:
-        return importlib.import_module(package_name)
-    except ImportError:
-        try:
-            # Try to install the package using pip in a subprocess
-            process = subprocess.run([sys.executable, "-m", "pip", "install", package_name], check=True)
-            if process.returncode != 0:
-                raise Exception("pip installation failed")
-            return importlib.import_module(package_name)
-        except Exception as exc:
-            print(f"Failed to install and import '{package_name}': {exc}")
-            raise
-
-screeninfo = psychopy_import("screeninfo")
 
 
 def ask_for_confirmation(second_monitor=False):
@@ -54,8 +30,8 @@ def ask_for_confirmation(second_monitor=False):
     response = messagebox.askyesno(
         title = "Alles startklar?",
         message = """
-        - Kalibrierung durchgeführt?\n
-        - Thermoino angeschlossen?\n
+        - MMS Programm umgestellt?\n
+        - Sensor Preview geöffnet?\n
         - ... ?""",
         parent=root)
     if response:
@@ -67,3 +43,16 @@ def ask_for_confirmation(second_monitor=False):
 
     root.destroy()
     return response
+
+
+def rgb255_to_rgb_psychopy(rgb255):
+    """
+    Convert an RGB color from 0-255 scale to 0-1 scale.
+
+    Args:
+    color_255 (tuple): A tuple of three integers representing the RGB color in 0-255 scale.
+
+    Returns:
+    tuple: A tuple of three floats representing the RGB color in 0-1 scale.
+    """
+    return [(x / 127.5) - 1 for x in rgb255]
