d0ccedcbddbc8e1c996a091c73fd5b9c5db2f72c
visserle <leonard.visser@mailbox.org>
2024-01-09 13:02:12 +0100

    Update process_raw.py

--- src/data/process_raw.py
+++ src/data/process_raw.py
@@ -11,42 +11,46 @@ from src.data.info_data import DataInfo
 @dataclass
 class Data:
     name: str
-    data: pd.DataFrame
+    dataset: pd.DataFrame # single imotions csv file
 
 @dataclass
 class Participant:
     id: str
-    signals: Dict[str, Data]
+    datasets: Dict[str, Data]
 
     def to_dataframe(self) -> pd.DataFrame:
-        data_frames = [signal.data for signal in self.signals.values()]
+        data_frames = [data.dataset for data in self.datasets.values()]
+        # Use reduce to merge all DataFrames on 'Timestamp'
         merged_df = reduce(
+            # Use reduce to merge all DataFrames on 'Timestamp'
             lambda left, right: pd.merge(left, right, on='Timestamp', how='outer'),
             data_frames
         )
         return merged_df.sort_values(by=['Timestamp'])
 
 
-def load_data(signal_path: str, data_info: DataInfo) -> Data:
-    with open(signal_path, 'r') as file:
-        lines = file.readlines(2**16)
+def load_data(data_path: str, data_info: DataInfo) -> Data:
+    # Find start index for data
+    with open(data_path, 'r') as file:
+        lines = file.readlines(2**16) # only read a few lines
     data_start_index = next(i for i, line in enumerate(lines) if "#DATA" in line)
-
-    data = pd.read_csv(
-        signal_path, 
+    
+    # Load and process data
+    dataset = pd.read_csv(
+        data_path, 
         skiprows=data_start_index + 1,
         usecols=lambda column: column in data_info.keep_columns,
     )
-    return Data(name=data_info.name, data=data)
+    return Data(name=data_info.name, dataset=dataset)
 
 def load_participant_data(participant_id: str, base_path: str, data_infos: List[DataInfo]) -> Participant:
     participant_path = os.path.join(base_path, participant_id)
-    signals: Dict[str, Data] = {}
+    datasets: Dict[str, Data] = {}
     for data_info in data_infos:
         file_path = os.path.join(participant_path, data_info.path)
         if os.path.exists(file_path):
-            signals[data_info.name] = load_data(file_path, data_info)
-    return Participant(id=participant_id, signals=signals)
+            datasets[data_info.name] = load_data(file_path, data_info)
+    return Participant(id=participant_id, datasets=datasets)
 
 def main():
     pass
