23a2b3dd4ab22b38791d56c89494c11853a5a988
visserle <leonard.visser@mailbox.org>
2024-01-12 15:02:21 +0100

    Update config_raw.py

--- src/data/config_raw.py
+++ src/data/config_raw.py
@@ -5,10 +5,11 @@ from src.data.config_data import DataConfigBase
 
 
 @dataclass
-class DataConfig(DataConfigBase):
+class RawConfig(DataConfigBase):
     name: str
-    use_columns: List[str]
+    load_columns: List[str]
     plot_columns: Optional[List[str]] = None
+    transformations: Optional[List] = None
     notes: Optional[str] = None
     
     def __post_init__(self):
@@ -16,58 +17,58 @@ class DataConfig(DataConfigBase):
         self.save_dir = Path('data/trial')
 
 
-TRIAL = DataConfig(
+TRIAL = RawConfig(
     name = 'trial',
-    use_columns = ["Timestamp","Stimuli_Seed"],
+    load_columns = ["Timestamp","Stimuli_Seed"],
     notes = ""
 )
 
-TEMPERATURE = DataConfig(
+TEMPERATURE = RawConfig(
     name = 'temperature',
-    use_columns = ["Timestamp","Temperature"],
+    load_columns = ["Timestamp","Temperature"],
     plot_columns = ["Temperature"],
 )
 
-RATING = DataConfig(
+RATING = RawConfig(
     name = 'rating',
-    use_columns = ["Timestamp","Rating"],
+    load_columns = ["Timestamp","Rating"],
     plot_columns = ["Rating"],
 )
 
-EDA = DataConfig(
+EDA = RawConfig(
     name = 'eda',
-    use_columns = ["Timestamp","EDA_RAW","EDA_d_Battery","EDA_d_PacketReceptionRate"],
+    load_columns = ["Timestamp","EDA_RAW","EDA_d_Battery","EDA_d_PacketReceptionRate"],
     plot_columns = ["EDA_RAW"],
 )
 
-ECG = DataConfig(
+ECG = RawConfig(
     name = 'ecg',
-    use_columns = ["Timestamp","ECG_LL-RA","ECG_LA-RA","ECG_Vx-RL","ECG_LL-RA_HeartRate","ECG_LL-RA_IBI","ECG_d_Battery","ECG_d_PacketReceptionRate"],
+    load_columns = ["Timestamp","ECG_LL-RA","ECG_LA-RA","ECG_Vx-RL","ECG_LL-RA_HeartRate","ECG_LL-RA_IBI","ECG_d_Battery","ECG_d_PacketReceptionRate"],
     plot_columns = ["ECG_LL-RA","ECG_LA-RA","ECG_Vx-RL","ECG_LL-RA_HeartRate","ECG_LL-RA_IBI"],
 )
 
-EEG = DataConfig(
+EEG = RawConfig(
     name = 'eeg',
-    use_columns = ["Timestamp","EEG_RAW_Ch1","EEG_RAW_Ch2","EEG_RAW_Ch3","EEG_RAW_Ch4","EEG_RAW_Ch5","EEG_RAW_Ch6","EEG_RAW_Ch7","EEG_RAW_Ch8"],
+    load_columns = ["Timestamp","EEG_RAW_Ch1","EEG_RAW_Ch2","EEG_RAW_Ch3","EEG_RAW_Ch4","EEG_RAW_Ch5","EEG_RAW_Ch6","EEG_RAW_Ch7","EEG_RAW_Ch8"],
     plot_columns = ["EEG_RAW_Ch1","EEG_RAW_Ch2","EEG_RAW_Ch3","EEG_RAW_Ch4","EEG_RAW_Ch5","EEG_RAW_Ch6","EEG_RAW_Ch7","EEG_RAW_Ch8"],
 )
 
-PUPILLOMETRY = DataConfig(
+PUPILLOMETRY = RawConfig(
     name = 'pupillometry',
-    use_columns = ["Timestamp","Pupillometry_L","Pupillometry_R","Pupillometry_L_Distance","Pupillometry_R_Distance"],
+    load_columns = ["Timestamp","Pupillometry_L","Pupillometry_R","Pupillometry_L_Distance","Pupillometry_R_Distance"],
     plot_columns=["Pupillometry_L","Pupillometry_R"],
 )
 
-AFFECTIVA = DataConfig(
+AFFECTIVA = RawConfig(
     name = 'affectiva',
-    use_columns = ["Timestamp","Anger","Contempt","Disgust","Fear","Joy","Sadness","Surprise","Engagement","Valence","Sentimentality","Confusion","Neutral","Attention","Brow Furrow","Brow Raise","Cheek Raise","Chin Raise","Dimpler","Eye Closure","Eye Widen","Inner Brow Raise","Jaw Drop","Lip Corner Depressor","Lip Press","Lip Pucker","Lip Stretch","Lip Suck","Lid Tighten","Mouth Open","Nose Wrinkle","Smile","Smirk","Upper Lip Raise","Blink","BlinkRate","Pitch","Yaw","Roll","Interocular Distance"],
+    load_columns = ["Timestamp","Anger","Contempt","Disgust","Fear","Joy","Sadness","Surprise","Engagement","Valence","Sentimentality","Confusion","Neutral","Attention","Brow Furrow","Brow Raise","Cheek Raise","Chin Raise","Dimpler","Eye Closure","Eye Widen","Inner Brow Raise","Jaw Drop","Lip Corner Depressor","Lip Press","Lip Pucker","Lip Stretch","Lip Suck","Lid Tighten","Mouth Open","Nose Wrinkle","Smile","Smirk","Upper Lip Raise","Blink","BlinkRate","Pitch","Yaw","Roll","Interocular Distance"],
     plot_columns = ["Anger","Contempt","Disgust","Fear","Joy","Sadness","Surprise","Engagement","Valence","Sentimentality","Confusion","Neutral","Attention","Brow Furrow","Brow Raise","Cheek Raise","Chin Raise","Dimpler","Eye Closure","Eye Widen","Inner Brow Raise","Jaw Drop","Lip Corner Depressor","Lip Press","Lip Pucker","Lip Stretch","Lip Suck","Lid Tighten","Mouth Open","Nose Wrinkle","Smile","Smirk","Upper Lip Raise","Blink","BlinkRate","Pitch","Yaw","Roll","Interocular Distance"],
     notes = "Affectiva data is not sampled at a constant rate and the only orignal data that can contain NaNs.",
 )
 
-SYSTEM = DataConfig(
+SYSTEM = RawConfig(
     name = 'system',
-    use_columns = ["Timestamp","CPU Sys","Memory Sys","CPU Proc","Memory Proc"],
+    load_columns = ["Timestamp","CPU Sys","Memory Sys","CPU Proc","Memory Proc"],
 )
 
 DATA_DICT = {
