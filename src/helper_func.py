ea0fbf2dc48b64e69958ae708e9c49638db27d3d
visserle <leonard.visser@mailbox.org>
2024-01-17 21:23:16 +0100

    Delete helper_func.py

--- src/helper_func.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-def flatten_list(l):
-    """Flatten a list that is one level deep (i.e., a list of lists)."""
-    return [item for sublist in l for item in sublist]
-
-def print_size(obj):
-    """Print the size of a DataFrame in MB."""
-    print('obj size:', round(sys.getsizeof(obj) / 1024**2), 'MB')
