8fc49b73a6d350d526719e006f9b837fbbe0a5d9
visserle <leonard.visser@mailbox.org>
2024-01-31 15:54:20 +0100

    calibration draft with text elements

--- src/expyriment/calibration.py
+++ src/expyriment/calibration.py
@@ -14,6 +14,7 @@ def press_space():
     """Press space to continue."""
     exp.keyboard.wait(keys=misc.constants.K_SPACE)
 
+
 control.set_develop_mode(True)
 control.defaults.window_size = (800, 600)
 design.defaults.experiment_background_colour = misc.constants.C_DARKGREY
@@ -40,69 +41,82 @@ for key in script.keys():
 for text in script.values():
     text.preload()
     
-    
+
+correction_after_preexposure = 2.0
 # Initialize estimator for VAS 70
 temp_start_vas70 = 40.0
-trials = 7
-estimator_vas70 = BayesianEstimatorVAS(
-    vas_value=70, 
-    temp_start=temp_start_vas70, 
-    temp_std=3.5, 
-    trials=trials
-    )
+trials_vas70 = 7
+trials_vas0 = 5
+
 
 control.start(skip_ready_screen=True)
 
-# script["welcome_1"].present()
-# stimuli.Tone(duration=1000, frequency=4400).play()
-# press_space()
-# script["welcome_2"].present()
-# press_space()
-# script["welcome_3"].present()
-# press_space()
-# script["info_preexposure"].present()
-# press_space()
-# stimuli.FixCross(colour=misc.constants.C_WHITE).present()
-# press_space()
-# script["question_preexposure"].present()
-# found, _ = exp.keyboard.wait(keys=[misc.constants.K_y, misc.constants.K_n])
-# if found == misc.constants.K_y:
-#     script["answer_yes"].present()
-# elif found == misc.constants.K_n:
-#     script["answer_no"].present()
-"""# If response was yes
-python_logging.info("Preexposure painful? Answer: %s", response_preexposure.keys)
-
-if response_preexposure.keys == "y":
-    # Decrease starting temperature
-    global temp_start_vas70 # psychopy has to be able to find it in the spaghetti
+script["welcome_1"].present()
+stimuli.Tone(duration=1000, frequency=4400).play()
+press_space()
+script["welcome_2"].present()
+press_space()
+script["welcome_3"].present()
+press_space()
+script["info_preexposure"].present()
+press_space()
+stimuli.FixCross(colour=misc.constants.C_WHITE).present()
+press_space()
+script["question_preexposure"].present()
+found, _ = exp.keyboard.wait(keys=[misc.constants.K_y, misc.constants.K_n])
+if found == misc.constants.K_y:
+    logging.info("Preexposure was already painful.")
     temp_start_vas70 -= correction_after_preexposure
-    # Reinitialize estimator for VAS 70 with different temp_start
-    global estimator_vas70
-    estimator_vas70 = BayesianEstimatorVAS(
-        vas_value=70,
-        temp_start=temp_start_vas70,
-        temp_std=temp_std_vas70,
-        trials=trials_vas70)"""
-# misc.Clock().wait(1000)
+    script["answer_yes"].present()
+elif found == misc.constants.K_n:
+    logging.info("Preexposure was not painful.")
+    script["answer_no"].present()
+misc.Clock().wait(1000)
 script["info_vas70_1"].present()
 press_space()
 script["info_vas70_2"].present()
 press_space()
 script["info_vas70_3"].present()
+estimator_vas70 = BayesianEstimatorVAS(
+    vas_value=70, 
+    temp_start=temp_start_vas70, 
+    temp_std=3.5, 
+    trials=trials_vas70
+    )
 press_space()
+
 
 for trial in range(estimator_vas70.trials):
     script["question_vas70"].present()
     found, _ = exp.keyboard.wait(keys=[misc.constants.K_y, misc.constants.K_n])
     if found == misc.constants.K_y:
-        response = "y"
-        estimator_vas70.conduct_trial(response=response,trial=trial)
+        estimator_vas70.conduct_trial(response="y",trial=trial)
         script["answer_yes"].present()
     elif found == misc.constants.K_n:
-        response = "n"
-        estimator_vas70.conduct_trial(response=response,trial=trial)
+        estimator_vas70.conduct_trial(response="n",trial=trial)
         script["answer_no"].present()
     misc.Clock().wait(1000)
+
+script["info_vas0"].present()
+estimator_vas0 = BayesianEstimatorVAS(
+    vas_value=0, 
+    temp_start=estimator_vas70.get_estimaten() - 3, # TODO
+    temp_std=3.5, 
+    trials=trials_vas0
+    )
+press_space()
+
+
+for trial in range(estimator_vas0.trials):
+    script["question_vas0"].present()
+    found, _ = exp.keyboard.wait(keys=[misc.constants.K_y, misc.constants.K_n])
+    if found == misc.constants.K_y:
+        estimator_vas0.conduct_trial(response="y",trial=trial)
+        script["answer_yes"].present()
+    elif found == misc.constants.K_n:
+        estimator_vas0.conduct_trial(response="n",trial=trial)
+        script["answer_no"].present()
+    misc.Clock().wait(1000)
+script["bye"].present()
 
 control.end()
