50c880ab8b57de0171603f8214844fd1392e2c8b
visserle <leonard.visser@mailbox.org>
2024-02-01 22:54:22 +0100

    fix structure

--- src/expyriment/utils.py
+++ src/expyriment/utils.py
@@ -1,5 +1,6 @@
 import re
 import tkinter as tk
+
 import toml
 import yaml
 from expyriment import stimuli
@@ -20,10 +21,60 @@ def load_script(file_path):
 def prepare_stimuli(script, box_size, text_size):
     """Convert script strings to TextBox stimuli and preload them."""
     for key, value in script.items():
-        script[key] = CustomTextBox(
-            text=value, size=box_size, position=[0, 0], text_size=text_size
-        )
+        script[key] = CustomTextBox(text=value, size=box_size, position=[0, 0], text_size=text_size)
         script[key].preload()
+
+
+class CustomTextBox(stimuli.TextBox):
+    """
+    This class is a copy of the TextBox class from expyriment.stimuli.textbox
+    with the only difference that it does not strip leading whitespace from the text.
+    This allows simpler text formatting of the script file using a constant text box size.
+
+    This code has been commented out twice:
+    # while lines and not lines[0]:
+    #     del lines[0]
+    """
+
+    def format_block(self, block):
+        """Format the given block of text.
+
+        This function is trimming leading and trailing
+        empty lines and any leading whitespace that is common to all lines.
+
+        Parameters
+        ----------
+        block : str
+            block of text to be formatted
+
+        """
+
+        # Separate block into lines
+        # lines = str(block).split('\n')
+        lines = block.split("\n")
+
+        # Remove leading/trailing empty lines
+        # while lines and not lines[0]:
+        #     del lines[0]
+        while lines and not lines[-1]:
+            del lines[-1]
+
+        # Look at first line to see how much indentation to trim
+        try:
+            ws = re.match(r"\s*", lines[0]).group(0)
+        except Exception:
+            ws = None
+        if ws:
+            lines = [x.replace(ws, "", 1) for x in lines]
+
+        # Remove leading/trailing blank lines (after leading ws removal)
+        # We do this again in case there were pure-whitespace lines
+        # while lines and not lines[0]:
+        #     del lines[0]
+        while lines and not lines[-1]:
+            del lines[-1]
+        return "\n".join(lines) + "\n"
+    # End of code taken from the word-wrapped text display module
 
 
 def warn_signal():
@@ -91,57 +142,6 @@ def scale_box_size(screen_size, base_box_size=(1500, 900), base_screen_size=(192
     return (scaled_box_width, scaled_box_height)
 
 
-class CustomTextBox(stimuli.TextBox):
-    """
-    This class is a copy of the TextBox class from expyriment.stimuli.textbox 
-    with the only difference that it does not strip leading whitespace from the text.
-    This allows simpler text formatting of the script file using a constant text box size.
-    
-    This code has been commented out twice:
-    # while lines and not lines[0]:
-    #     del lines[0]
-    """
-    def format_block(self, block):
-        """Format the given block of text.
-
-        This function is trimming leading and trailing
-        empty lines and any leading whitespace that is common to all lines.
-
-        Parameters
-        ----------
-        block : str
-            block of text to be formatted
-
-        """
-        
-        # Separate block into lines
-        #lines = str(block).split('\n')
-        lines = block.split('\n')
-
-        # Remove leading/trailing empty lines
-        # while lines and not lines[0]:
-        #     del lines[0]
-        while lines and not lines[-1]:
-            del lines[-1]
-
-        # Look at first line to see how much indentation to trim
-        try:
-            ws = re.match(r'\s*', lines[0]).group(0)
-        except Exception:
-            ws = None
-        if ws:
-            lines = [x.replace(ws, '', 1) for x in lines]
-
-        # Remove leading/trailing blank lines (after leading ws removal)
-        # We do this again in case there were pure-whitespace lines
-        # while lines and not lines[0]:
-        #     del lines[0]
-        while lines and not lines[-1]:
-            del lines[-1]
-        return '\n'.join(lines) + '\n'
-    # End of code taken from the word-wrapped text display module
-
-
 if __name__ == "__main__":
     """Cheap test for the text scaling functions."""
     from expyriment import control, design
@@ -180,4 +180,3 @@ if __name__ == "__main__":
 
     # Keep the window open until a key is pressed
     exp.keyboard.wait()
-
