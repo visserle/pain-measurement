f989e4ab7631c320b0af5538e9a49ddd2bf751c3
visserle <leonard.visser@mailbox.org>
2024-02-01 15:46:12 +0100

    Update utils.py
    add center window func

--- src/expyriment/utils.py
+++ src/expyriment/utils.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 import toml
 import yaml
 from expyriment import stimuli
@@ -25,3 +26,15 @@ def prepare_stimuli(script):
 def warn_signal():
     """Play a warn signal."""
     stimuli.Tone(duration=500, frequency=440).play()
+
+
+def center_window(window: tk.Tk):
+    """Center a window on the primary screen."""
+    window.update_idletasks()
+    width = window.winfo_width()
+    height = window.winfo_height()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    center_x = int(screen_width / 2 - width / 2)
+    center_y = int(screen_height / 2 - height / 2)
+    window.geometry(f"+{center_x}+{center_y}")
