e54542a94a1f4ff4149e0cfa57f707d15c738d3e
visserle <leonard.visser@mailbox.org>
2024-02-02 10:33:14 +0100

    Update participant_data.py

--- src/expyriment/participant_data.py
+++ src/expyriment/participant_data.py
@@ -14,7 +14,23 @@ import pandas as pd
 logger = logging.getLogger(__name__.rsplit(".", maxsplit=1)[-1])
 
 
+COLUMN_HEADERS = [
+    "time_stamp",
+    "id",
+    "age",
+    "gender",
+    "vas0",
+    "vas70",
+    "baseline_temp",
+    "temp_range",
+]
+
+
 class ParticipantDataApp:
+    """
+    A simple GUI for entering participant data. Programmed as a class to allow for easy access to the participant_info dict.
+    """
+
     def __init__(self, master):
         self.master = master
         self.participant_info = {}
@@ -79,7 +95,7 @@ class ParticipantDataApp:
         self.master.destroy()
 
 
-def ask_for_participant_info():
+def ask_for_participant_info() -> dict:
     root = tk.Tk()
     root.withdraw()  # Hide window initially
     app = ParticipantDataApp(root)
@@ -90,22 +106,12 @@ def ask_for_participant_info():
 
 
 def init_excel_file(file_path):
-    headers = [
-        "time_stamp",
-        "id",
-        "age",
-        "gender",
-        "vas0",
-        "vas70",
-        "baseline_temp",
-        "temp_range",
-    ]
     if not file_path.exists():
-        df = pd.DataFrame(columns=headers)
+        df = pd.DataFrame(columns=COLUMN_HEADERS)
         df.to_excel(file_path, index=False)
 
 
-def complete_participant_info(participant_info: dict):
+def complete_participant_info(participant_info: dict) -> dict:
     """
     Add additional information to the participant_info dict:
     - time_stamp
@@ -144,17 +150,7 @@ def add_participant_info(file_path, participant_info: dict) -> dict:
         participant_info = complete_participant_info(participant_info)
 
     # Create a dataframe with the same order as the participants.xlsx file
-    new_order = [
-        "time_stamp",
-        "id",
-        "age",
-        "gender",
-        "vas0",
-        "vas70",
-        "baseline_temp",
-        "temp_range",
-    ]
-    participant_info_df = pd.DataFrame([participant_info], columns=new_order)
+    participant_info_df = pd.DataFrame([participant_info], columns=COLUMN_HEADERS)
 
     # Append participant info to the participants.xlsx file
     participants_xlsx = pd.read_excel(file_path)
@@ -171,11 +167,11 @@ def add_participant_info(file_path, participant_info: dict) -> dict:
     # Save the updated participants.xlsx file
     participants_xlsx.to_excel(file_path, index=False)
     logger.info(f"Added participant {participant_info['id']} to {file_path}")
-    
+
     return participant_info
 
 
-def read_last_participant(file_path):  # TODO FIXME
+def read_last_participant(file_path) -> dict:
     """
     Returns information about the last participant from the participants.xlsx file.
 
@@ -187,27 +183,18 @@ def read_last_participant(file_path):  # TODO FIXME
     participant_info = read_last_participant()
     ```
     """
-    df = pd.read_excel(file_path)
-    last_row = df.iloc[-1]
-
-    participant_info = {
-        "time_stamp": last_row["time_stamp"],
-        "participant": last_row["participant"],
-        "age": last_row["age"],
-        "gender": last_row["gender"],
-        "baseline_temp": last_row["baseline_temp"],
-        "temp_range": last_row["temp_range"],
-    }
+    last_row = pd.read_excel(file_path).iloc[-1]
+    participant_info = last_row.to_dict()
 
     # Check if the participant data is from today
     today = datetime.now().strftime("%Y-%m-%d")
     if today not in participant_info["time_stamp"]:
         logger.warning(
-            f"Participant data from {participant_info['participant']} ({participant_info['time_stamp']}) is not from today."
+            f"Participant data from {participant_info['id']} ({participant_info['time_stamp']}) is not from today."
         )
 
     logger.info(
-        f"Participant data from {participant_info['participant']} ({participant_info['time_stamp']}) loaded."
+        f"Participant data from {participant_info['id']} ({participant_info['time_stamp']}) loaded."
     )
 
     return participant_info
