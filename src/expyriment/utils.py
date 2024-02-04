8fb91aece96e49a2ea97ad9cad935999bf14b917
visserle <leonard.visser@mailbox.org>
2024-02-05 00:20:06 +0100

    declutter utils

--- src/expyriment/utils.py
+++ src/expyriment/utils.py
@@ -1,13 +1,15 @@
 import logging
-import re
-import tkinter as tk
-from tkinter import messagebox, ttk
 
 import toml
 import yaml
 from expyriment import stimuli
 
+from src.expyriment.custom_text_box import CustomTextBox
+
 logger = logging.getLogger(__name__.rsplit(".", maxsplit=1)[-1])
+
+
+BASE_SCREEN_SIZE = (1920, 1200)
 
 
 def load_configuration(file_path):
@@ -22,64 +24,26 @@ def load_script(file_path):
         return yaml.safe_load(file)
 
 
-def prepare_script(script, box_size, text_size):
-    """Convert script strings to CustomTextBox stimuli and preload them."""
+def prepare_script(script, text_size, text_box_size, parent_key=None):
+    """
+    Recursively convert script strings to CustomTextBox stimuli and preload them.
+
+    (With special preloading for 'instruction' as they are shown with the visual analogue scale composition)
+    """
     for key, value in script.items():
-        script[key] = CustomTextBox(text=value, size=box_size, position=[0, 0], text_size=text_size)
-        script[key].preload()
-
-
-class CustomTextBox(stimuli.TextBox):
-    """
-    This class is a copy of the TextBox class from expyriment.stimuli.textbox
-    with the only difference that it does not strip leading whitespace from the text.
-    This allows simpler text formatting of the script file using a constant text box size.
-
-    This code has been commented out twice:
-    # while lines and not lines[0]:
-    #     del lines[0]
-    """
-
-    def format_block(self, block):
-        """Format the given block of text.
-
-        This function is trimming leading and trailing
-        empty lines and any leading whitespace that is common to all lines.
-
-        Parameters
-        ----------
-        block : str
-            block of text to be formatted
-
-        """
-
-        # Separate block into lines
-        # lines = str(block).split('\n')
-        lines = block.split("\n")
-
-        # Remove leading/trailing empty lines
-        # while lines and not lines[0]:
-        #     del lines[0]
-        while lines and not lines[-1]:
-            del lines[-1]
-
-        # Look at first line to see how much indentation to trim
-        try:
-            ws = re.match(r"\s*", lines[0]).group(0)
-        except Exception:
-            ws = None
-        if ws:
-            lines = [x.replace(ws, "", 1) for x in lines]
-
-        # Remove leading/trailing blank lines (after leading ws removal)
-        # We do this again in case there were pure-whitespace lines
-        # while lines and not lines[0]:
-        #     del lines[0]
-        while lines and not lines[-1]:
-            del lines[-1]
-        return "\n".join(lines) + "\n"
-
-    # End of code taken from the word-wrapped text display module
+        if isinstance(value, dict):
+            # Recursively process nested dictionaries, passing down the current key as the parent_key
+            prepare_script(value, text_size, text_box_size, key)
+        else:
+            # Convert strings to CustomTextBox stimuli
+            script[key] = CustomTextBox(
+                text=value, size=text_box_size, position=[0, 0], text_size=text_size
+            )
+            # Preload the stimuli
+            if parent_key != "instruction":
+                script[key].preload()
+            else:
+                script[key].preload(inhibit_ogl_compress=True)
 
 
 def warn_signal():
@@ -87,69 +51,7 @@ def warn_signal():
     stimuli.Tone(duration=500, frequency=440).play()
 
 
-def center_tk_window(window: tk.Tk):
-    """Center a window on the primary screen."""
-    window.update_idletasks()
-    width = window.winfo_width()
-    height = window.winfo_height()
-    screen_width = window.winfo_screenwidth()
-    screen_height = window.winfo_screenheight()
-    center_x = int(screen_width / 2 - width / 2)
-    center_y = int(screen_height / 2 - height / 2)
-    window.geometry(f"+{center_x}+{center_y}")
-    return (center_x, center_y)
-
-
-def ask_for_measurement_start() -> bool:
-    """Custom confirmation dialog window with checkboxes for each item."""
-    root = tk.Tk()
-    dialog = ChecklistDialog(root)
-    response = dialog.show()
-    if response:
-        print("Confirmation for experiment start received.")
-    else:
-        print("Confirmation for experiment start denied.")
-    return response
-
-
-class ChecklistDialog:
-    def __init__(self, root):
-        self.root = root
-        self.response = False  # Default response is False
-
-        # Set up the window
-        self.root.title("Alles startklar?")
-        center_x, center_y = center_tk_window(self.root)
-        self.root.geometry(f"+{center_x}+{center_y}")
-
-        # Add checkboxes
-        self.check_vars = []  # To keep track of the Checkbutton variables
-        items = ["MMS Programm umgestellt?", "Sensor Preview geöffnet?"]
-        for item in items:
-            var = tk.BooleanVar()
-            chk = tk.Checkbutton(self.root, text=item, variable=var)
-            chk.pack(anchor="w", padx=20, pady=5)
-            self.check_vars.append(var)
-
-        # Add proceed button
-        self.proceed_button = ttk.Button(self.root, text="Proceed", command=self.on_proceed)
-        self.proceed_button.pack(pady=20)
-
-    def on_proceed(self):
-        # Check if all checkboxes are ticked
-        if all(var.get() for var in self.check_vars):
-            self.response = True
-        else:
-            messagebox.showwarning("Warning", "Please confirm all items before proceeding.")
-        if self.response:
-            self.root.destroy()
-
-    def show(self):
-        self.root.mainloop()
-        return self.response
-
-
-def scale_ratio(screen_size, base_screen_size=(1920, 1200)):
+def _scale_ratio(screen_size, base_screen_size=BASE_SCREEN_SIZE) -> float:
     """
     Calculate the scale ratio based on the screen size.
     """
@@ -160,80 +62,44 @@ def scale_ratio(screen_size, base_screen_size=(1920, 1200)):
     return scale_ratio
 
 
-def scale_text_size(screen_size, base_text_size=40, base_screen_size=(1920, 1200)):
+def scale_1d_value(
+    base_value: int | float,
+    screen_size: tuple[int, int],
+    base_screen_size: tuple[int, int] = BASE_SCREEN_SIZE,
+) -> int | float:
     """
-    Calculate the adjusted text size based on the screen size.
-    Base sizes are for reference only and can be changed to any value.
+    Calculate the adjusted value based on the screen size for 1D values like length, width or text size.
 
     Parameters:
+    - base_value: int or float, base value to scale from
     - screen_size: tuple, current screen size (width, height)
-    - base_screen_size: tuple, base screen size (width, height) for scaling reference
-    - base_text_size: int, base text size to scale from
+    - base_screen_size: tuple, base screen size (width, height) for scaling reference, default=(1920, 1200)
 
     Returns:
-    - scaled_text_size: int, scaled text size based on the current screen size
-
+    - scaled_value: int or float, scaled value based on the current screen size
     """
-    scale_factor = scale_ratio(screen_size, base_screen_size)
-    scaled_text_size = int(base_text_size * scale_factor)
-    return scaled_text_size
+    scale_factor = _scale_ratio(screen_size, base_screen_size)
+    if isinstance(base_value, int):
+        scaled_value = int(base_value * scale_factor)
+    else:
+        scaled_value = float(base_value * scale_factor)
+    return scaled_value
 
 
-def scale_box_size(screen_size, base_box_size=(1500, 900), base_screen_size=(1920, 1200)):
+def scale_2d_tuple(
+    base_value: tuple[int, int], screen_size, base_screen_size=BASE_SCREEN_SIZE
+) -> tuple[int, int]:
     """
-    Calculate the adjusted box size based on the screen size.
+    Calculate the adjusted value based on the screen size for 2D values like position or size.
 
     Parameters:
+    - base_value: tuple, base value to scale from (width, height)
     - screen_size: tuple, current screen size (width, height)
-    - base_screen_size: tuple, base screen size (width, height) for scaling reference
-    - base_box_size: tuple, base box size (width, height) to scale from
+    - base_screen_size: tuple, base screen size (width, height) for scaling reference, default=(1920, 1200)
 
     Returns:
-    - scaled_box_size: tuple, scaled box size based on the current screen size
+    - scaled_value: tuple, scaled value based on the current screen size
     """
-    scale_factor = scale_ratio(screen_size, base_screen_size)
-    scaled_box_width = int(base_box_size[0] * scale_factor)
-    scaled_box_height = int(base_box_size[1] * scale_factor)
-    return (scaled_box_width, scaled_box_height)
-
-
-if __name__ == "__main__":
-    ask_for_measurement_start()
-
-
-# if __name__ == "__main__":
-#     """Cheap test for the text scaling functions."""
-#     from expyriment import control, design
-
-#     # Example screen sizes
-#     size_a = (800, 600)
-#     size_b = (1024, 768)
-#     size_c = (1900, 1200)
-
-#     # Set the window size for the experiment
-#     control.defaults.window_size = size_c
-#     control.set_develop_mode(True)
-
-#     # Initialize the experiment and get the screen size
-#     exp = design.Experiment("Text Scaling Example")
-#     control.initialize(exp)
-#     screen_size = exp.screen.size
-
-#     # Calculate the scaled font size and box size
-#     scaled_font_size = scale_text_size(screen_size)
-#     scaled_box_size = scale_box_size(screen_size)
-
-#     # Create a text stimulus with the adjusted sizes
-#     text_stimulus = stimuli.TextBox(
-#         "Scaled\nText",
-#         size=scaled_box_size,
-#         position=(0, 0),
-#         text_size=scaled_font_size,
-#         background_colour=(255, 255, 255),
-#     )
-
-#     text_stimulus.preload()
-#     text_stimulus.present()
-
-#     # Keep the window open until a key is pressed
-#     exp.keyboard.wait()
+    scale_factor = _scale_ratio(screen_size, base_screen_size)
+    scaled_value = (int(base_value[0] * scale_factor), int(base_value[1] * scale_factor))
+    return scaled_value
