93bb0aae95224e4d652d7fe5e490fd772f9ddc4e
visserle <leonard.visser@mailbox.org>
2024-03-06 00:00:38 +0100

    add argparse, refactor and so on

--- src/questionnaires/app.py
+++ src/questionnaires/app.py
@@ -1,4 +1,5 @@
-from pathlib import Path
+import argparse
+import webbrowser
 
 import yaml
 from flask import Flask, redirect, render_template, request, url_for
@@ -6,38 +7,72 @@ from flask import Flask, redirect, render_template, request, url_for
 from src.log_config import configure_logging
 from src.questionnaires.functions import save_results, score_results
 
-configure_logging()
-
-scale = "bdi"
+configure_logging(ignore_libs=["werkzeug", "participant_data"])
 
 app = Flask(__name__)
 
-with open(f"src/questionnaires/inventory/{scale}.yaml", "r") as file:
-    questionnaire = yaml.safe_load(file)
+
+def load_questionnaire(scale):
+    with open(f"src/questionnaires/inventory/{scale}.yaml", "r") as file:
+        current_questionnaire = yaml.safe_load(file)
+    return current_questionnaire
 
 
-@app.route("/", methods=["GET", "POST"])
-def survey():
+@app.route("/")
+def home():
+    return redirect(url_for("questionnaire_handler", scale=questionnaires[0]))
+
+
+@app.route("/<scale>", methods=["GET", "POST"])
+def questionnaire_handler(scale):
+    current_questionnaire = load_questionnaire(scale)
     if request.method == "POST":
         answers = request.form
         score = score_results(scale, answers)
-        save_results(scale, questionnaire, answers, score)
-        return redirect(url_for("thank_you"))
+        save_results(scale, current_questionnaire, answers, score)
+
+        next_index = questionnaires.index(scale) + 1
+        if next_index < len(questionnaires):
+            return redirect(
+                url_for("questionnaire_handler", scale=questionnaires[next_index])
+            )
+        else:
+            return redirect(url_for("thank_you"))
+
     return render_template(
         f"{scale}.html",
-        questions=questionnaire["questions"],
-        options=questionnaire["options"] if "options" in questionnaire else None,
-        title=questionnaire["title"] if "title" in questionnaire else None,
-        instructions=questionnaire["instructions"]
-        if "instructions" in questionnaire
+        title=current_questionnaire["title"],
+        instructions=current_questionnaire["instructions"]
+        if "instructions" in current_questionnaire
+        else None,
+        questions=current_questionnaire["questions"],
+        options=current_questionnaire["options"]
+        if "options" in current_questionnaire
         else None,
     )
 
 
 @app.route("/thank_you")
 def thank_you():
-    return "Thank you for completing the survey!"
+    return "Vielen Dank für das Ausfüllen der Fragebögen!"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    QUESTIONNAIRES = ["bdi", "pcs"]
+
+    parser = argparse.ArgumentParser(
+        description="Run the app with selected questionnaires."
+    )
+    parser.add_argument(
+        "questionnaire",
+        nargs="*",
+        default=QUESTIONNAIRES,
+        help=f"Select the questionnaires to run ({', '.join(QUESTIONNAIRES)}).",
+    )
+    args = parser.parse_args()
+
+    global questionnaires
+    questionnaires = args.questionnaire
+
+    webbrowser.open_new("http://localhost:5000")
+    app.run(debug=False)
