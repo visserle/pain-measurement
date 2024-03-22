1927fbaee10c4be1d3e0f9c6f0af45949334a873
visserle <leonard.visser@mailbox.org>
2024-03-22 09:08:21 +0100

    Update pop_ups.py

--- src/experiments/measurement/pop_ups.py
+++ src/experiments/measurement/pop_ups.py
@@ -11,52 +11,6 @@ from src.helpers import center_tk_window
 logger = logging.getLogger(__name__.rsplit(".", maxsplit=1)[-1])
 
 
-class ChecklistDialog:
-    def __init__(self, root, items):
-        self.root = root
-        self.root.title("Ready to Start?")
-        self.items = items
-        self.response = False
-        self.setup_ui()
-
-    def setup_ui(self):
-        """Sets up the UI components of the dialog."""
-        self._create_checkboxes()
-        self._create_proceed_button()
-        self.root.deiconify()
-
-    def _create_checkboxes(self):
-        """Creates a checkbox for each item."""
-        self.check_vars = []
-        for item in self.items:
-            var = tk.BooleanVar()
-            chk = tk.Checkbutton(self.root, text=item, variable=var)
-            chk.pack(anchor="w", padx=20, pady=5)
-            self.check_vars.append(var)
-
-    def _create_proceed_button(self):
-        """Creates the proceed button."""
-        self.proceed_button = ttk.Button(
-            self.root, text="Proceed", command=self.on_proceed
-        )
-        self.proceed_button.pack(pady=20)
-
-    def on_proceed(self):
-        """Handles the proceed button click event."""
-        if all(var.get() for var in self.check_vars):
-            self.response = True
-            self.root.destroy()
-        else:
-            messagebox.showwarning(
-                "Warning", "Please confirm all items before proceeding."
-            )
-
-    def show(self):
-        """Displays the dialog and returns the user's response."""
-        self.root.mainloop()
-        return self.response
-
-
 def ask_for_eyetracker_calibration() -> bool:
     user_choice = {"proceed": False}  # Dictionary to store user's choice
 
@@ -107,6 +61,52 @@ def ask_for_measurement_start() -> bool:
     else:
         logger.debug("Confirmation for experiment start denied.")
     return response
+
+
+class ChecklistDialog:
+    def __init__(self, root, items):
+        self.root = root
+        self.root.title("Ready to Start?")
+        self.items = items
+        self.response = False
+        self.setup_ui()
+
+    def setup_ui(self):
+        """Sets up the UI components of the dialog."""
+        self._create_checkboxes()
+        self._create_proceed_button()
+        self.root.deiconify()
+
+    def _create_checkboxes(self):
+        """Creates a checkbox for each item."""
+        self.check_vars = []
+        for item in self.items:
+            var = tk.BooleanVar()
+            chk = tk.Checkbutton(self.root, text=item, variable=var)
+            chk.pack(anchor="w", padx=20, pady=5)
+            self.check_vars.append(var)
+
+    def _create_proceed_button(self):
+        """Creates the proceed button."""
+        self.proceed_button = ttk.Button(
+            self.root, text="Proceed", command=self.on_proceed
+        )
+        self.proceed_button.pack(pady=20)
+
+    def on_proceed(self):
+        """Handles the proceed button click event."""
+        if all(var.get() for var in self.check_vars):
+            self.response = True
+            self.root.destroy()
+        else:
+            messagebox.showwarning(
+                "Warning", "Please confirm all items before proceeding."
+            )
+
+    def show(self):
+        """Displays the dialog and returns the user's response."""
+        self.root.mainloop()
+        return self.response
 
 
 if __name__ == "__main__":
