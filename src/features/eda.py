ede2a6f19ecfd28ae169a12de2678c9ee5d43e16
visserle <leonard.visser@mailbox.org>
2024-03-25 06:30:23 +0100

    Update eda.py

--- src/features/eda.py
+++ src/features/eda.py
@@ -1,14 +1,19 @@
 import neurokit2 as nk
+import pandas as pd
 import polars as pl
 
 from src.features.transformations import map_trials
 
+EDA_RAW_COLUMN = "EDA_RAW"
+
 
 @map_trials
-def eda_process(df: pl.DataFrame, sampling_rate: int) -> pl.DataFrame:
-    eda_raw_np = df.select("EDA_RAW").to_numpy().flatten()
-    # Returns EDA_Phasic and EDA_Tonic as pd.DataFrame
-    eda_processed_pd = nk.eda_phasic(
-        eda_raw_np, sampling_rate=sampling_rate, method="neurokit"
-    )
-    return df.hstack(pl.from_pandas(eda_processed_pd))
+def process_eda(
+    df: pl.DataFrame,
+    sampling_rate: int,
+) -> pl.DataFrame:
+    eda_raw = df.select(EDA_RAW_COLUMN).to_numpy().flatten()
+    eda_processed: pd.DataFrame = nk.eda_phasic(
+        eda_raw, sampling_rate=sampling_rate, method="neurokit"
+    )  # returns EDA_Phasic and EDA_Tonic columns
+    return df.hstack(pl.from_pandas(eda_processed))
