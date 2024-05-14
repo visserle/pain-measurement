26d6db0eae5f9a65cdc112a0380c12e87fd1c617
visserle <leonard.visser@mailbox.org>
2024-05-14 14:10:58 +0200

    refactor tts

--- src/experiments/utils.py
+++ src/experiments/utils.py
@@ -6,9 +6,6 @@ from pathlib import Path
 import toml
 import yaml
 from expyriment.stimuli import Audio, TextBox
-from google.cloud import texttospeech
-from gtts import gTTS
-from openai import OpenAI
 
 BASE_SCREEN_SIZE = (1920, 1200)
 
@@ -125,6 +122,32 @@ def prepare_script(
                 script[key].preload()
 
 
+def prepare_audio(
+    audio: dict,
+    audio_dir: str,
+    parent_key: str = None,
+) -> None:
+    """
+    Recursively convert existings audio files to Audio stimuli and preload them,
+    based on the keys of the script dictionary.
+    """
+    for key, value in audio.items():
+        print(key)
+        if isinstance(value, dict):
+            # Recursively process nested dictionaries,
+            # passing down the current key as the parent_key
+            prepare_audio(value, audio_dir, parent_key=key)
+        else:
+            audio_path = (
+                Path(audio_dir) / f"{parent_key}_{key}.wav"
+                if parent_key
+                else Path(audio_dir) / f"{key}.wav"
+            ).as_posix()
+            print(audio_path)
+            audio[key] = Audio(audio_path)
+            audio[key].preload()
+
+
 class CustomTextBox(TextBox):
     """
     Expyriment text box without leading whitespace stripping.
@@ -176,129 +199,3 @@ class CustomTextBox(TextBox):
         while lines and not lines[-1]:
             del lines[-1]
         return "\n".join(lines) + "\n"
-
-
-def text_to_speech(
-    text: str,
-    output_path: str,
-    model: str,
-) -> None:
-    """
-    Generate and save an audio file from the given text.
-    """
-    # TODO: only keep one of the following implementations
-    match model:
-        case "openai":
-            client = OpenAI()
-            response = client.audio.speech.create(
-                model="tts-1-hd",
-                voice="alloy",
-                input=text,
-            )
-            response.write_to_file(output_path)
-        case "gtts":
-            tts = gTTS(text, lang="de")
-            tts.save(output_path)
-        case "google":
-            # Instantiates a client
-            client = texttospeech.TextToSpeechClient()
-
-            # Set the text input to be synthesized
-            synthesis_input = texttospeech.SynthesisInput(text=text)
-
-            # Build the voice request
-            voice = texttospeech.VoiceSelectionParams(
-                language_code="de-DE",
-                name="de-DE-Wavenet-B",
-            )
-
-            # Select the type of audio file you want returned
-            audio_config = texttospeech.AudioConfig(
-                audio_encoding=texttospeech.AudioEncoding.LINEAR16
-            )
-
-            # Perform the text-to-speech request on the text input with the selected
-            # voice parameters and audio file type
-            response = client.synthesize_speech(
-                input=synthesis_input,
-                voice=voice,
-                audio_config=audio_config,
-            )
-
-            # The response's audio_content is binary.
-            with open(output_path, "wb") as out:
-                # Write the response to the output file.
-                out.write(response.audio_content)
-    print("Audio content written to file:", output_path)
-
-
-def script_to_speech(
-    script: dict,
-    audio_dir: str,
-    parent_key=None,
-) -> None:
-    """
-    Generate audio files for all the script values.
-    """
-    for key, value in script.items():
-        if isinstance(value, dict):
-            # Recursively process nested dictionaries,
-            # passing down the current key as the parent_key
-            script_to_speech(
-                script=value,
-                audio_dir=audio_dir,
-                parent_key=key,
-            )
-        else:
-            # Remove '(y/n)' and '(Leertaste drücken, um fortzufahren)' from the text
-            value = re.sub(r"\(y/n\)", "", value)
-            value = re.sub(r"\(Leertaste drücken, um fortzufahren\)", "", value)
-
-            audio_path = (
-                Path(audio_dir) / f"{parent_key}_{key}.wav"
-                if parent_key
-                else Path(audio_dir) / f"{key}.wav"
-            )
-            audio_path.parent.mkdir(parents=True, exist_ok=True)
-            text_to_speech(value, audio_path, model="google")
-
-
-def main_tts():
-    """Generate audio files for the calibration and measurement scripts."""
-    experiments = ["calibration", "measurement"]
-    for exp in experiments:
-        # Load the script file
-        file_path = Path(f"src/experiments/{exp}/{exp}_script.yaml")
-        script = load_script(file_path)
-        # Create audio files for the script
-        script_to_speech(script, f"src/experiments/{exp}/audio")
-
-
-def prepare_audio(
-    audio: dict,
-    audio_dir: str,
-    parent_key: str = None,
-) -> None:
-    """
-    Recursively convert existings audio files to Audio stimuli and preload them,
-    based on the structure of the script dictionary.
-    """
-    for key, value in audio.items():
-        print(key)
-        if isinstance(value, dict):
-            # Recursively process nested dictionaries,
-            # passing down the current key as the parent_key
-            prepare_audio(value, audio_dir, parent_key=key)
-        else:
-            audio_path = (
-                Path(audio_dir) / f"{parent_key}_{key}.wav"
-                if parent_key
-                else Path(audio_dir) / f"{key}.wav"
-            ).as_posix()
-            print(audio_path)
-            audio[key] = Audio(audio_path)
-            audio[key].preload()
-
-
-if __name__ == "__main__":
-    main_tts()
