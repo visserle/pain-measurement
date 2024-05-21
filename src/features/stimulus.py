8c6698a1699b3bad3ebc1361c8d5d4769ed748a8
visserle <leonard.visser@mailbox.org>
2024-05-21 16:49:48 +0200

    Update stimulus.py

--- src/features/stimulus.py
+++ src/features/stimulus.py
@@ -1,6 +1,27 @@
 import polars as pl
 
 from src.features.aggregation import calculate_corr
+from src.features.scaling import scale_min_max, scale_percent_to_decimal
+
+
+def process_stimulus(df: pl.DataFrame) -> pl.DataFrame:
+    """
+    Normalize the 'Stimulus' data by scaling the 'Rating' and 'Temperature' columns to
+    [0, 1].
+    """
+    df = scale_rating(df)
+    df = scale_temperature(df)
+    return df
+
+
+def scale_rating(df: pl.DataFrame) -> pl.DataFrame:
+    """Normalize the 'Rating' column to the range [0, 1] by dividing by 100."""
+    return scale_percent_to_decimal(df, exclude_columns=["Temperature"])
+
+
+def scale_temperature(df: pl.DataFrame) -> pl.DataFrame:
+    """Normalize the 'Temperature' column using min-max scaling (for each trial)."""
+    return scale_min_max(df, exclude_columns=["Rating"])
 
 
 def corr_temperature_rating(df: pl.DataFrame) -> pl.DataFrame:
