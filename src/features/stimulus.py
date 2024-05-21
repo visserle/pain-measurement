e623665e8fa26cc04eda03b48d445dce8311d821
visserle <leonard.visser@mailbox.org>
2024-05-21 13:57:59 +0200

    Update stimulus.py

--- src/features/stimulus.py
+++ src/features/stimulus.py
@@ -5,12 +5,12 @@ from src.features.transformations import map_trials
 
 @map_trials
 def calculate_corr(df: pl.DataFrame) -> pl.DataFrame:
-    correlation_df = (
+    """Calculate the correlation between 'Rating' and 'Temperature' for each trial."""
+    return (
         df.select(["Rating", "Temperature"])
         .corr()
         .gather_every(2)  # corr method returns a 2x2 table
         .rename({"Rating": "Trial", "Temperature": "Correlation"})
-        .with_columns(pl.Series("Trial", [df["Trial"][0]]))
+        .with_columns(pl.Series("Trial", [df["Trial"][0]]))  # add trial number back
         .with_columns(pl.col("Correlation").round(2))
     )
-    return correlation_df
