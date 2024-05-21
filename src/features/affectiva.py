bda589da7640c4334088e17f26db18521e291b13
visserle <leonard.visser@mailbox.org>
2024-05-21 12:49:29 +0200

    refactor scaling

--- src/features/affectiva.py
+++ src/features/affectiva.py
@@ -1,28 +1,9 @@
 # Note: domain range is 0-1, scale accordingly
 import polars as pl
 
-from src.features.transformations import map_trials
-
-EXCLUDE_COLUMNS = [
-    "Timestamp",
-    "SampleNumber",
-    "Trial",
-    "Stimulus_Seed",
-    "Participant",
-    "Trial",
-]
+from src.features.scaling import scale_percent_to_decimal
 
 
-@map_trials
-def process_affectiva(
-    df: pl.DataFrame,
-    exclude_columns: list[str] = EXCLUDE_COLUMNS,
-) -> pl.DataFrame:
+def process_affectiva(df: pl.DataFrame) -> pl.DataFrame:
     """Normalize each affectiva feature to the range [0, 1] by dividing by 100."""
-    return df.with_columns(
-        _scale_100_col(pl.col(pl.Float64).exclude(exclude_columns))
-    )  # TODO: trial shouldn't even be float64
-
-
-def _scale_100_col(col: pl.Expr) -> pl.Expr:
-    return col / 100
+    return scale_percent_to_decimal(df)
---
+++ src/features/scaling.py
@@ -0,0 +1,60 @@
+import polars as pl
+
+from src.features.transformations import map_trials
+
+EXCLUDE_COLUMNS = [
+    "Timestamp",
+    "SampleNumber",
+    "Trial",
+    "Stimulus_Seed",
+    "Participant",
+    "Trial",
+]
+
+
+def _scale_min_max_col(col: pl.Expr) -> pl.Expr:
+    return (col - col.min()) / (col.max() - col.min())
+
+
+def _scale_standard_col(col: pl.Expr) -> pl.Expr:
+    return (col - col.mean()) / col.std()
+
+
+def _scale_percent_to_decimal_col(col: pl.Expr) -> pl.Expr:
+    return col / 100
+
+
+@map_trials
+def scale_min_max(
+    df: pl.DataFrame,
+    exclude_columns: list[str] = EXCLUDE_COLUMNS,
+) -> pl.DataFrame:
+    """NOTE: Not for usage in ML pipeline (data leakage)."""
+    # TODO: trial shouldn't even be float64
+    return df.with_columns(
+        _scale_min_max_col(pl.col(pl.Float64).exclude(exclude_columns))
+    )
+
+
+@map_trials
+def scale_standard(
+    df: pl.DataFrame,
+    exclude_columns: list[str] = EXCLUDE_COLUMNS,
+) -> pl.DataFrame:
+    """NOTE: Not for usage in ML pipeline (data leakage)."""
+    # TODO: trial shouldn't even be float64
+    return df.with_columns(
+        _scale_standard_col(pl.col(pl.Float64).exclude(exclude_columns))
+    )
+
+
+@map_trials
+def scale_percent_to_decimal(
+    df: pl.DataFrame,
+    exclude_columns: list[str] = EXCLUDE_COLUMNS,
+) -> pl.DataFrame:
+    """Scale percentage columns to decimal."""
+    # TODO: trial shouldn't even be float64
+    return df.with_columns(
+        _scale_percent_to_decimal_col(pl.col(pl.Float64).exclude(exclude_columns))
+    )
