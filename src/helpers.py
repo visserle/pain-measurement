db95a020a08ef2777779b94ede8897dd45d71cbf
visserle <leonard.visser@mailbox.org>
2024-05-22 17:39:03 +0200

    refactor polars to_describe helper function

--- src/helpers.py
+++ src/helpers.py
@@ -1,6 +1,7 @@
 import ast
 import tkinter as tk
 
+import polars as pl
 from screeninfo import get_monitors
 
 
@@ -19,6 +20,25 @@ def convert_str_to_list(str_list: str) -> list:
         return ast.literal_eval(str_list)
     except ValueError:
         return []
+
+
+def to_describe(col, prefix=""):
+    """
+    Helper function for having the describe expression for use in groupby-agg.
+    From https://github.com/pola-rs/polars/issues/8066#issuecomment-1794144838
+    """
+    prefix = prefix or f"{col}_"
+    return [
+        pl.col(col).count().alias(f"{prefix}count"),
+        pl.col(col).is_null().sum().alias(f"{prefix}null_count"),
+        pl.col(col).mean().alias(f"{prefix}mean"),
+        pl.col(col).std().alias(f"{prefix}std"),
+        pl.col(col).min().alias(f"{prefix}min"),
+        pl.col(col).quantile(0.25).alias(f"{prefix}25%"),
+        pl.col(col).quantile(0.5).alias(f"{prefix}50%"),
+        pl.col(col).quantile(0.75).alias(f"{prefix}75%"),
+        pl.col(col).max().alias(f"{prefix}max"),
+    ]
 
 
 def center_tk_window(
