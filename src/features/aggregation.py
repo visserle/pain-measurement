1b836a323bc2fbfbed488efe2af9901d701a8a83
visserle <leonard.visser@mailbox.org>
2024-05-23 11:13:38 +0200

    refactor

--- src/features/aggregation.py
+++ src/features/aggregation.py
@@ -2,13 +2,14 @@ import polars as pl
 
 from src.features.transformations import map_trials
 
+ADDITIONAL_INFO_COLUMNS = ["Trial", "Participant", "Stimulus_Seed", "Skin_Area"]
+
 
 @map_trials
 def calculate_corr(
     df: pl.DataFrame,
     feature1: str,
     feature2: str,
-    add_columns: list[str] = ["Trial", "Participant", "Stimulus_Seed"],
 ) -> pl.DataFrame:
     """
     Calculate the correlation between two features (columns of a DataFrame) for each
@@ -19,11 +20,10 @@ def calculate_corr(
         .corr()
         .gather_every(2)  # corr method returns a 2x2 table
         .rename({feature1: "Trial", feature2: "Correlation"})  # repurpose the columns
-        .with_columns(  # add the columns that were passed in if available
-            # using list comprehension
+        .with_columns(  # add additional info columns using list comprehension
             [
                 pl.Series(add_column, [df[add_column][0]])
-                for add_column in add_columns
+                for add_column in ADDITIONAL_INFO_COLUMNS
                 if add_column in df.columns
             ]
         )
