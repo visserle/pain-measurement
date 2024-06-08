4e780bd51138a213989b169d659c20067d05ae4e
visserle <leonard.visser@mailbox.org>
2024-06-08 13:27:30 +0200

    Update test_stimulus_function.py

--- tests/experiments/measurement/test_stimulus_function.py
+++ tests/experiments/measurement/test_stimulus_function.py
@@ -4,8 +4,17 @@ from src.experiments.measurement.stimulus_generator import StimulusGenerator
 from src.experiments.utils import load_configuration
 
 config = load_configuration(Path("src/experiments/measurement/measurement_config.toml"))
-stimulus = StimulusGenerator(config["stimulus"])
+config = config["stimulus"]
+stimulus = StimulusGenerator(config)
 
 
 def test_stimulus_duration():
     assert stimulus.duration == 180
+
+
+def test_uniqueness_of_seeds():
+    assert len(config["seeds"]) == len(set(config["seeds"]))
+
+
+def test_number_of_seeds():
+    assert len(config["seeds"]) == 12
