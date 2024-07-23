9c8a13a6cbe10330985f8d9561ef5c5b224fbe10
visserle <leonard.visser@mailbox.org>
2024-07-23 08:15:11 +0200

    Update database_manager.py

--- src/data/database_manager.py
+++ src/data/database_manager.py
@@ -3,20 +3,21 @@ from pathlib import Path
 
 import duckdb
 import polars as pl
-from imotions_processing import (
+
+from src.data.imotions_data import (
     create_raw_data_dfs,
     create_trials_df,
     load_imotions_data,
 )
-
 from src.data.imotions_data_config import data_config
+from src.data.seeds_data import seed_data  # noqa (used in query)
 from src.data.utils import pl_schema_to_duckdb_schema
 
 logger = logging.getLogger(__name__.rsplit(".", maxsplit=1)[-1])
 
 # Paths
-db_file = Path("experiment_tertete.duckdb")
-participant_data_file = Path("runs/experiments/participants.csv")
+DB_FILE = Path("experiment.duckdb")
+participant_data = Path("runs/experiments/participants.csv")
 data = Path("data/imotions")
 
 
@@ -27,9 +28,9 @@ class DatabaseSchema:
 
     @staticmethod
     def create_trials_table(
-        con: duckdb.DuckDBPyConnection,
+        conn: duckdb.DuckDBPyConnection,
     ) -> None:
-        con.execute("""
+        conn.execute("""
             CREATE SEQUENCE IF NOT EXISTS seq_trials_trial_id START 1;
             CREATE TABLE IF NOT EXISTS Trials (
                 trial_id USMALLINT NOT NULL DEFAULT NEXTVAL('seq_trials_trial_id'),
@@ -45,16 +46,19 @@ class DatabaseSchema:
         """)
 
     @staticmethod
-    def create_seeds_table():
-        pass
+    def create_seeds_table(
+        conn: duckdb.DuckDBPyConnection,
+    ) -> None:
+        # Load directly into the database
+        conn.execute("CREATE OR REPLACE TABLE Seeds AS SELECT * FROM seed_data")
 
     @staticmethod
     def create_raw_data_table(
-        con: duckdb.DuckDBPyConnection,
+        conn: duckdb.DuckDBPyConnection,
         name: str,
         schema: pl.Schema,
     ) -> None:
-        con.execute(f"""
+        conn.execute(f"""
             CREATE TABLE IF NOT EXISTS {name} (
                 trial_id USMALLINT,
                 {pl_schema_to_duckdb_schema(schema)},
@@ -67,20 +71,39 @@ class DatabaseSchema:
 class DatabaseManager:
     def __init__(
         self,
-        db_file: Path,
+        db_file: str | Path = DB_FILE,
+        auto_connect: bool = True,
     ):
         self.db_file = db_file
-        self.con = None
+        self.conn = None
+        if auto_connect:  # no need to call connect() explicitly or via context manager
+            self.connect()
+
+    def __enter__(self):
+        if not self.conn:
+            self.connect()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.disconnect()
 
     def connect(self):
-        self.con = duckdb.connect(str(self.db_file))
-        # DatabaseSchema.create_participants_table(self.con)  # TODO
-        DatabaseSchema.create_trials_table(self.con)
+        self.conn = duckdb.connect(str(self.db_file))
+        # DatabaseSchema.create_participants_table(self.conn)  # TODO
+        DatabaseSchema.create_trials_table(self.conn)
+        DatabaseSchema.create_seeds_table(self.conn)
 
     def disconnect(self):
-        if self.con:
-            self.con.close()
-            self.con = None
+        if self.conn:
+            self.conn.close()
+            self.conn = None
+
+    def execute(self, query: str):
+        """Execute a query on the database. Does not close the connection."""
+        try:
+            return self.conn.execute(query)
+        except AttributeError:
+            self.connect()
+            return self.execute(query)
 
     def load_trials(
         self,
@@ -88,7 +111,7 @@ class DatabaseManager:
     ):
         columns = ", ".join(trials_df.columns)
         try:
-            self.con.execute(f"INSERT INTO Trials ({columns}) SELECT * FROM trials_df")
+            self.conn.execute(f"INSERT INTO Trials ({columns}) SELECT * FROM trials_df")
         except duckdb.ConstraintException as e:
             logger.warning(f"Trial data already exists in the database: {e}")
 
@@ -97,42 +120,31 @@ class DatabaseManager:
         name: str,
         raw_data_df: pl.DataFrame,
     ):
-        DatabaseSchema.create_raw_data_table(self.con, name, raw_data_df.schema)
-        self.con.execute(f"""
+        DatabaseSchema.create_raw_data_table(self.conn, name, raw_data_df.schema)
+        self.conn.execute(f"""
             INSERT INTO {name}
             SELECT t.trial_id, r.*
             FROM raw_data_df AS r
-            JOIN Trials AS t ON r.trial_number = t.trial_number AND r.participant_id = t.participant_id;
+            JOIN Trials AS t ON r.trial_number = t.trial_number AND r.participant_id = t.participant_id
+            ORDER BY r.rownumber;
         """)
 
 
 def main():
-    db_manager = DatabaseManager(Path("your_database_file.db"))
-    db_manager.connect()
-
-    try:
+    db = DatabaseManager()
+    with db:
         for participant_id in range(1, 24):
             # Load polars dataframes into dictionary from config
-            load_imotions_data(
-                data_config,
-                data,
-                participant_id=participant_id,
-            )
+            load_imotions_data(data_config, data, participant_id=participant_id)
 
             # Create metadata table
-            trials = create_trials_df(
-                data_config,
-                participant_id=participant_id,
-            )
-            db_manager.load_trials(trials)
+            trials = create_trials_df(data_config, participant_id=participant_id)
+            db.load_trials(trials)
 
             # Create raw data tables
             raw_data_dfs = create_raw_data_dfs(data_config, trials)
             for key, df in raw_data_dfs.items():
-                db_manager.load_raw_data(key, df)
-
-    finally:
-        db_manager.disconnect()
+                db.load_raw_data(key, df)
 
 
 if __name__ == "__main__":
