ea43cd9c50e9db41b1bf008ea3cd62e97323f3a5
visserle <leonard.visser@mailbox.org>
2024-07-29 16:01:58 +0200

    Update plotting_utils.py

--- src/visualization/plotting_utils.py
+++ src/visualization/plotting_utils.py
@@ -20,16 +20,19 @@ from src.data.config_participant import ParticipantConfig
 from src.data.make_dataset import load_dataset
 
 
-def add_nan_to_trials_for_hvplot(
+def prepare_multiline_hvplot(
     df: pl.DataFrame,
-    time_column: str,
+    time_column: str = "timestamp",
+    trial_column: str = "trial_id",
 ):
     """
-    Add NaN separators to a DataFrame for each trial to prepare the data for plotting.
+    Add NaN separators at the end of each trial to prepare plotting of multiple lines
+    per category in hvplot.
 
-    See: https://hvplot.holoviz.org/user_guide/Large_Timeseries.html#multiple-lines-per-category-example
+    https://hvplot.holoviz.org/user_guide/Large_Timeseries.html#multiple-lines-per-category-example
     """
 
+    # Define columns where we don't want to add NaN separators
     info_columns = [
         "trial_id",
         "participant_id",
@@ -39,26 +42,26 @@ def add_nan_to_trials_for_hvplot(
     ]
 
     # Create a new DataFrame with NaN rows
-    group_by_columns = [column for column in df.columns if column in info_columns]
+    group_by_columns = [x for x in df.columns if x in info_columns]
 
     nan_df = df.group_by(group_by_columns).agg(
         [
-            pl.lit(float("nan")).alias(col)
+            pl.lit(float("nan")).alias(col)  # following hvplot docs, not using pl.Null
             for col in df.columns
             if col not in info_columns
         ]
     )
 
-    # Add a temporary column for sorting
+    # Add a temporary column for sorting to add NaN separators at the end of each trial
     df = df.with_columns(pl.lit(0).alias("temp_sort"))
     nan_df = nan_df.with_columns(pl.lit(1).alias("temp_sort"))
 
     # Concatenate the original DataFrame with the NaN DataFrame
     result = pl.concat(
         [df, nan_df],
-        how="diagonal",
+        how="diagonal_relaxed",  # FIXME: maybe we can do better than just ignoring the schema
         rechunk=True,
-    ).sort(["continuous_interval_id", "temp_sort", time_column])
+    ).sort([trial_column, "temp_sort", time_column])
 
     # Remove the temporary sorting column and return the result
     return result.drop("temp_sort")
