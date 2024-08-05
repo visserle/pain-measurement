40cc5b2ca918aa7cb96b723c6c72eefbc8d478fe
visserle <leonard.visser@mailbox.org>
2024-08-05 06:42:56 +0200

    Delete affectiva.py

--- src/features/affectiva.py
+++
@@ -1,8 +0,0 @@
-import polars as pl
-
-from src.features.scaling import scale_percent_to_decimal
-
-
-def process_affectiva(df: pl.DataFrame) -> pl.DataFrame:
-    """Normalize each affectiva feature to the range [0, 1] by dividing by 100."""
-    return scale_percent_to_decimal(df)
