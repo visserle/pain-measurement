e34bec23c65885408208085f92995e0722ccac52
visserle <leonard.visser@mailbox.org>
2024-08-21 09:40:17 +0200

    Update face.py

--- src/features/face.py
+++ src/features/face.py
@@ -4,11 +4,24 @@ from src.features.scaling import scale_percent_to_decimal
 
 
 def preprocess_face(df: pl.DataFrame) -> pl.DataFrame:
-    # Normalize each affectiva feature to the range [0, 1] by dividing by 100.
-    df = scale_percent_to_decimal(df)
+    df = scale_face(df)
     return df
 
 
 def feature_face(df: pl.DataFrame) -> pl.DataFrame:
     # Drop irrelevant columns
     return df
+
+
+def scale_face(df: pl.DataFrame) -> pl.DataFrame:
+    """
+    Normalize the facial expression columns to the range [0, 1] by dividing by 100.
+    """
+    return scale_percent_to_decimal(
+        df,
+        exclude_additional_columns=[
+            "blink",
+            "blinkrate",
+            "interocular_distance",
+        ],
+    )
