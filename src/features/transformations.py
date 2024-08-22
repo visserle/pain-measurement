ecb94998fc5855edd6b878f9e28c2f58adde460a
visserle <leonard.visser@mailbox.org>
2024-08-22 14:28:25 +0200

    rm remove duplicate ts fn

--- src/features/transformations.py
+++ src/features/transformations.py
@@ -51,37 +51,6 @@ def map_participants(func: callable):
     return wrapper
 
 
-@map_trials
-def remove_duplicate_timestamps(
-    df: pl.DataFrame,
-) -> pl.DataFrame:
-    """
-    Remove duplicate timestamps from the DataFrame.
-
-    For instance, the Shimmer3 GSR+ unit collects 128 samples per second but with
-    only 100 unique timestamps. This function removes the duplicates.
-
-    Note that timestamps can be duplicated across participants, so we need to group by
-    trial_id (or participant_id) before removing duplicates.
-    """
-    return df.unique("timestamp").sort("timestamp")
-
-
-def add_time_column(
-    df: pl.DataFrame,
-) -> pl.DataFrame:
-    """
-    Create a new column that contains the time from Timestamp in ms.
-
-    Note: This datatype is not fully implemented in Polars and DuckDB yet and is not
-    recommended for saving to a database.
-    """
-    df = df.with_columns(
-        pl.col("timestamp").cast(pl.Duration(time_unit="ms")).alias("time")
-    )
-    return df
-
-
 """
 NOTE: NEW: scikit-learn’s transformers now support polars output with the set_output API.
 
