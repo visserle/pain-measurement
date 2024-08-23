b7d6e4f3522c6d0989301fef3d6aed144484e170
visserle <leonard.visser@mailbox.org>
2024-08-23 14:57:45 +0200

    Update stimulus.py

--- src/features/stimulus.py
+++ src/features/stimulus.py
@@ -4,6 +4,8 @@ from src.features.scaling import scale_min_max, scale_percent_to_decimal
 
 
 def preprocess_stimulus(df: pl.DataFrame) -> pl.DataFrame:
+    df = scale_rating(df)
+    df = scale_temperature(df)
     return df
 
 
@@ -11,32 +13,23 @@ def feature_stimulus(df: pl.DataFrame) -> pl.DataFrame:
     return df
 
 
-# REMOVE TODO
-def process_stimulus(df: pl.DataFrame) -> pl.DataFrame:
-    """
-    Normalize the 'Stimulus' data by scaling the 'Rating' and 'Temperature' columns to
-    [0, 1].
-
-    NOTE: This function should not be used in the ML pipeline due to data leakage of
-    the 'Temperature' column. However, we don't yet if want to use 'Temperature' as a
-    target, so this function is included for now. Also the data leakeage is not
-    significant. TODO
-    """
-    df = scale_rating(df)
-    df = scale_temperature(df)
-    return df
-
-
 def scale_rating(df: pl.DataFrame) -> pl.DataFrame:
-    """Normalize the 'Rating' column to the range [0, 1] by dividing by 100."""
-    return scale_percent_to_decimal(df, exclude_additional_columns=["Temperature"])
+    """Normalize the 'rating' column to the range [0, 1] by dividing by 100."""
+    return scale_percent_to_decimal(df, exclude_additional_columns=["temperature"])
 
 
 def scale_temperature(df: pl.DataFrame) -> pl.DataFrame:
-    """Normalize the 'Temperature' column using min-max scaling (for each trial).
+    """Normalize the 'temperature' column using min-max scaling (for each trial).
 
     NOTE: This function should not be used in the ML pipeline due to data leakage of
-    the 'Temperature' column. However, we don't yet if want to use 'Temperature' as a
+    the 'temperature' column. However, we don't yet if want to use 'temperature' as a
     target, so this function is included for now. Also the data leakeage is not
-    significant. TODO"""
-    return scale_min_max(df, exclude_additional_columns=["Rating"])
+    significant. TODO: actually the range is known beforehand, so it's not a problem.
+
+    NOTE: scale_min_max automatically maps to trials.
+
+    TODO, BUG: the actual min and max values should be retrieved from the calibration
+    results, not from the data itself. (low priority, bc temperature is not used as a
+    target)
+    """
+    return scale_min_max(df, exclude_additional_columns=["rating"])
