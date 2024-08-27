3e74a7c7b9485c117a57ebaa5b456c7a3bb75acd
visserle <leonard.visser@mailbox.org>
2024-08-27 14:30:53 +0200

    Update stimulus_generator.py

--- src/experiments/measurement/stimulus_generator.py
+++ src/experiments/measurement/stimulus_generator.py
@@ -1,3 +1,9 @@
+# NOTE: There is one small detail I missed: The resulting curve should have been
+# normalized to [-1, 1] for smooth maximum values as determined by the calibration.
+# However, the resulting difference is negligible (for a max value of 47.75 °C, the mean
+# of the resulting max values over all seeds is 47.7369 °C). There is no difference in
+# the mean of the min values.
+
 import numpy as np
 import scipy
 
