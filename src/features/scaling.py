34e743da2ab39dd74d02dda6ca926e3ecde03c6e
visserle <leonard.visser@mailbox.org>
2024-09-17 14:08:32 +0200

    add robust scaler

--- src/features/scaling.py
+++ src/features/scaling.py
@@ -46,6 +46,22 @@ def scale_standard(
     )
 
 
+@map_trials
+def scale_robust_standard(
+    df: pl.DataFrame,
+    exclude_additional_columns: list[str] | None = None,
+) -> pl.DataFrame:
+    """
+    Scale Float64 columns to have median 0 and median absolute deviation 1 for each trial.
+
+    Note: Should be used with caution in ML pipelines to avoid **data leakage**.
+    """
+    exclude_columns = EXCLUDE_COLUMNS + (exclude_additional_columns or [])
+    return df.with_columns(
+        _scale_robust_standard_col(pl.col(pl.Float64).exclude(exclude_columns))
+    )
+
+
 # does not need to be mapped since it's a simple operation
 def scale_percent_to_decimal(
     df: pl.DataFrame,
@@ -69,6 +85,10 @@ def _scale_min_max_col(column: pl.Expr) -> pl.Expr:
 
 def _scale_standard_col(column: pl.Expr) -> pl.Expr:
     return (column - column.mean()) / column.std()
+
+
+def _scale_robust_standard_col(column: pl.Expr) -> pl.Expr:
+    return (column - column.median()) / (column.quantile(0.75) - column.quantile(0.25))
 
 
 def _scale_percent_to_decimal_col(column: pl.Expr) -> pl.Expr:
