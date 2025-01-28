f557199dd638d67602141ed716c46737f13fc760
visserle <leonard.visser@mailbox.org>
2025-01-28 16:27:29 +0100

    Update face.py

--- src/features/face.py
+++ src/features/face.py
@@ -1,22 +1,14 @@
-# TODO: more explicit would be to use with column names instead of exclude additional columns
-# TODO: maybe upsample for equidistant time points?
-
-"""
-cited after: Kappesser J. 2019 "The facial
-expression of pain in humans considered from
-a social perspective"
-typically involve the lowering of the eyebrows, squeezing of the eyes, wrinkling of the nose, raising of the upper lip and
-opening of the mouth (Craig KC, Prkachin KM, Grunau RE. 2011), which can be observed for
-clinical and experimental pain (Williams ACdeC. 2002 Facial expression of pain: an
-evolutionary account. Behav. Brain Sci. 25, 4.
-(doi:10.1017/S0140525X02000080))
-"""  # TODO REMOVE
-
 import polars as pl
 
 from src.features.scaling import scale_percent_to_decimal
 
-SAMPLE_RATE = 10  # rounded up from 9.76 (0.76% coefficient of variation between trials)
+FEATURE_COLUMNS = [
+    "brow_furrow",
+    "cheek_raise",
+    "mouth_open",
+    "upper_lip_raise",
+    "nose_wrinkle",
+]
 
 
 def preprocess_face(df: pl.DataFrame) -> pl.DataFrame:
@@ -25,7 +17,8 @@ def preprocess_face(df: pl.DataFrame) -> pl.DataFrame:
 
 
 def feature_face(df: pl.DataFrame) -> pl.DataFrame:
-    df = df.drop(non_feature_columns)
+    df = df.select(FEATURE_COLUMNS)
+    # no need to decimate as the sample rate is already at 10 Hz
     return df
 
 
@@ -41,41 +34,3 @@ def scale_face(df: pl.DataFrame) -> pl.DataFrame:
             "interocular_distance",
         ],
     )
-
-
-non_feature_columns = [
-    "anger",
-    "contempt",
-    "disgust",
-    "fear",
-    "joy",
-    "sadness",
-    "surprise",
-    "engagement",
-    "valence",
-    "sentimentality",
-    "confusion",
-    "neutral",
-    "attention",
-    "brow_raise",
-    "chin_raise",
-    "dimpler",
-    "eye_closure",
-    "eye_widen",
-    "inner_brow_raise",
-    "jaw_drop",
-    "lip_corner_depressor",
-    "lip_press",
-    "lip_pucker",
-    "lip_stretch",
-    "lip_suck",
-    "lid_tighten",
-    "smile",
-    "smirk",
-    "blink",
-    "blinkrate",
-    "pitch",
-    "yaw",
-    "roll",
-    "interocular_distance",
-]
