b3461858f412af69881ec36662d33e32ad46670e
visserle <leonard.visser@mailbox.org>
2025-02-08 16:14:57 +0100

    Update utils.py

--- src/models/utils.py
+++ src/models/utils.py
@@ -2,15 +2,16 @@ import logging
 
 import torch
 
+logger = logging.getLogger(__name__.rsplit(".", 1)[-1])
+
 
 def get_device() -> torch.device:
     """Return the device to be used by the model."""
-    device = (
-        torch.device("cuda")
-        if torch.cuda.is_available()
-        else torch.device("mps")
-        if torch.backends.mps.is_available()
-        else torch.device("cpu")
-    )
-    logging.info(f"Using device: {device}")
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
+    logger.info(f"Using device: {device}")
     return device
