0b976ca29277652599b5e23af6f5b18c3fdf3b73
visserle <leonard.visser@mailbox.org>
2025-02-13 17:53:14 +0100

    Update confidence_intervals.py

--- src/plots/confidence_intervals.py
+++ src/plots/confidence_intervals.py
@@ -1,13 +1,25 @@
+"""
+Plot confidence intervals for the given modality for all participants for each stimulus seed.
+
+Uses 95% confidence interval (1.96) for the mean of a normal distribution.
+(95% chance your population mean will fall between lower and upper limit)
+"""
+
+import logging
+
 import hvplot.polars
 import polars as pl
 from icecream import ic
 from polars import col
 
-from src.features.resampling import add_timestamp_μs_column
+from src.features.resampling import add_normalized_timestamp, add_timestamp_μs_column
 from src.features.scaling import scale_min_max, scale_robust_standard, scale_standard
 
 BIN_SIZE = 1  # seconds
 CONFIDENCE_LEVEL = 1.96  # 95% confidence interval
+
+
+logger = logging.getLogger(__name__.rsplit(".", 1)[-1])
 
 
 def plot_confidence_intervals(
@@ -41,13 +53,13 @@ def plot_confidence_intervals(
     return plots
 
 
-def prepare_confidence_intervals(
+def create_confidence_intervals(
     df: pl.DataFrame,
     signals: list[str],
     bin_size: int = BIN_SIZE,
 ) -> pl.DataFrame:
     """
-    Prepare data with confidence intervals for visualization.
+    Data with confidence intervals for visualization.
     """
     # Scale data for better visualization (mapped over trial_id)
     df = scale_min_max(
@@ -58,9 +70,9 @@ def prepare_confidence_intervals(
         ],
     )
 
-    # Calculate confidence intervals
+    # Create confidence intervals
     df = aggregate_over_stimulus_seeds(df, signals, bin_size=bin_size)
-    return add_confidence_interval(df, signals)
+    return calculate_confidence_intervals(df, signals)
 
 
 def aggregate_over_stimulus_seeds(
@@ -69,25 +81,25 @@ def aggregate_over_stimulus_seeds(
     bin_size: int = BIN_SIZE,
 ) -> pl.DataFrame:
     """Aggregate over stimulus seeds for each trial using group_by_dynamic."""
-    # Note: without group_by_dynamic, this would be something like
-    # ````
-    # df.with_columns(
-    #     [(col("zeroed_timestamp") // 1000).cast(pl.Int32).alias("time_bin")]
-    #     )
-    #     .group_by(["stimulus_seed", "time_bin"])
-    # ````
 
     # Zero-based timestamp in milliseconds
-    df = _zero_based_timestamps(df)
+    df = add_normalized_timestamp(df)
     # Add microsecond timestamp column for better precision as group_by_dynamic uses int
-    df = add_timestamp_μs_column(df, "zeroed_timestamp")
+    df = add_timestamp_μs_column(df, "normalized_timestamp")
     # Time binning
     return (
         (
-            df.sort("zeroed_timestamp_µs")
+            df.sort("normalized_timestamp_µs")
+            # Note: without group_by_dynamic, this would be something like
+            # ````
+            # df.with_columns(
+            #   [(col("normalized_timestamp") // 1000).cast(pl.Int32).alias("time_bin")]
+            #   )
+            #   .group_by(["stimulus_seed", "time_bin"])
+            # ````
             .group_by_dynamic(
-                "zeroed_timestamp_µs",
-                every=f"{int((1000 / (1/bin_size))*1000)}i",
+                "normalized_timestamp_µs",
+                every=f"{int((1000 / (1 / bin_size)) * 1000)}i",
                 group_by=["stimulus_seed"],
             )
             .agg(
@@ -104,26 +116,25 @@ def aggregate_over_stimulus_seeds(
                 + [pl.len().alias("sample_size")]
             )
         )
-        .with_columns((col("zeroed_timestamp_µs") / 1_000_000).alias("time_bin"))
+        .with_columns((col("normalized_timestamp_µs") / 1_000_000).alias("time_bin"))
         .sort("stimulus_seed", "time_bin")
         # remove measures at exactly 180s so that they don't get their own bin
         .filter(col("time_bin") < 180)
-        .drop("zeroed_timestamp_µs")
+        .drop("normalized_timestamp_µs")
     )
 
 
-def _zero_based_timestamps(df: pl.DataFrame) -> pl.DataFrame:
-    return df.with_columns(
-        (col("timestamp") - col("timestamp").min().over("trial_id")).alias(
-            "zeroed_timestamp"
-        )
-    )
-
-
-def add_confidence_interval(
+def calculate_confidence_intervals(
     df: pl.DataFrame,
     signals: str,
+    min_sample_size: int = 30,
 ) -> pl.DataFrame:
+    small_samples = df.filter(col("sample_size") < min_sample_size)
+    if small_samples.height > 0:
+        logger.warn(
+            f"Warning: {small_samples.height} bins have sample size < {min_sample_size}"
+        )
+
     return df.with_columns(
         [
             (
